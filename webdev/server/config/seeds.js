8487e5b530fdf8eff8be6fd72fdf9dc1a76a46d2
Trevor Bruce <tbnyk03@gmail.com>
2021-07-22 18:40:11 -0600

    Latest Pull

--- webdev/server/config/seeds.js
+++ webdev/server/config/seeds.js
@@ -1,2 +1,17 @@
 const db = require('./connection');
-const
+const { User, Profile } = require ('../models');
+
+db.once('open', async () => {
+    await Profile.deletemany();
+
+    const profiles = await Profile.insertMany([
+        {  name: 'Andreas'},
+        {  name: 'Brett' },
+        {  name: 'Dingle' },
+    ]);
+    console.log("Profiles have been seeded");
+
+    await Profile.deletemany();
+
+    const profiles =
+})
