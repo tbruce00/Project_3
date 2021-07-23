bb37265e2a09f900d3981c71f440b0460fd94d30
micah shumaker <micah.shumaker@gmail.com>
2021-07-22 20:23:04 -0600

    pushing profile-card

--- webdev/src/App.js
+++ webdev/src/App.js
@@ -1,13 +1,13 @@
 import Profile from './components/Profile';
 import './App.css';
+// import signUp from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
       <Profile
-      
-      
       />
+      
 
     </div>
   );
