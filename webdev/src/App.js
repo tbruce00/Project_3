62af2c932d071273c3075e7df78aced8471cca48
micah shumaker <micah.shumaker@gmail.com>
2021-07-22 21:14:13 -0600

    jumbotron prototype

--- webdev/src/App.js
+++ webdev/src/App.js
@@ -1,10 +1,12 @@
 import Profile from './components/Profile';
+import Jumbotron from './components/Jumbotron';
 import './App.css';
 // import signUp from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
+      <Jumbotron />
       <Profile
       />
       
