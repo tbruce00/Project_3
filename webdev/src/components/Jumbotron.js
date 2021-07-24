5939d51675a3b4261b745db97c9cd4d22d654924
micah shumaker <micah.shumaker@gmail.com>
2021-07-24 12:07:16 -0600

    pushing jumbotron to main

--- webdev/src/components/Jumbotron.js
+++ webdev/src/components/Jumbotron.js
@@ -3,15 +3,18 @@ import './Jumbotron.css';
 
 const Jumbotron = () => {
   return (
-    <div class="jumbotron">
-    <div class="container">
-      <h1>Sample Name</h1>
-      <p>
-      This is a platform for viewing or creating WebDev profiles.  
+    <div className="jumbotron">
+    <div className="container">
+      <h1>Sample Name </h1>
+      <p className="jumbotronText" >
+      This is a platform for viewing or creating WebDev profiles. 
+       
+      <button class="button"><span>Create Profile </span></button>
       </p>
-      <p>
+      <p className="jumbotronText">
       Browse Web Devs for your next job, or add your name to the list for future employers to see!
       </p>
+      
     </div>
   </div>
    
