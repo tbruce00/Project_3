a93078b7f8bc03fb15dd4ed26eb8e896ffe2c76e
CWhirl <68519564+CWhirl@users.noreply.github.com>
2021-07-27 22:57:10 -0600

    SignUp component init

--- Project_3/client/src/components/Jumbotron/Jumbotron.js
+++ Project_3/client/src/components/Jumbotron/Jumbotron.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './Jumbotron.css';
 import codeSnippet from './images/codesnippetscreenshot.png'
+import SignUp from '../SignUp/signup';
 const Jumbotron = () => {
   
   
@@ -9,7 +10,7 @@ const Jumbotron = () => {
     <div className="container">
       <h1>Sample Name </h1>
       <img src={codeSnippet} />
-      <button class="button"> makeProfile<span className="parenthesisColor" >()</span> </button>
+      <SignUp />
       <p className="jumbotronText">
       Browse Web Devs for your next job, or add your name to the list for future employers to see!
       </p>
--- Project_3/client/src/components/SignUp/signup.js
+++ Project_3/client/src/components/SignUp/signup.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import firebase from 'firebase';
-import auth from 'firebase/auth';
-
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 function SignUp(props) {
+    function getUserInfo(token) {
+        // get the user info the the github graphql api
+        var url = "https://api.github.com/graphql";
+        var headers = {
+          "Authorization": "Bearer " + token
+        };
+        var options = {
+          method: "POST",
+          headers: headers,
+          body: JSON.stringify({
+            query: "query { viewer { name email location login url websiteUrl bio avatarUrl} }"
+          })
+        };
+        return fetch(url, options).then(function(response) {
+          return response.json();
+      });
+    }
     const handleSubmit = async (event) => {
         event.preventDefault();
         const provider = new firebase.auth.GithubAuthProvider();
         firebase.auth().signInWithPopup(provider).then(function(result) {
             // This gives you a GitHub Access Token. You can use it to access the GitHub API.
             const token = result.credential.accessToken;
-            const user = firebase.auth(token).currentUser;
-            const updateDB = (token, user.uid);
-            // console.log(getUserInfo(token));
+            console.log(getUserInfo(token));
           });
         };
-
     return (
         <form onSubmit={handleSubmit}>
-        <button type="submit" class="button"><span>Create Profile </span></button>
+        <button class="button"> makeProfile<span className="parenthesisColor" >()</span> </button>
         </form>
     )
 }
